e6e50236f905d5c13a6a96b8b19e5b2d2f0d21e6
Kruzyabra <kruzyabra@yandex.ru>
2016-08-11 00:37:05 +0300

    Добавлены фрагменты для каждого из пунктов меню. Переход по нажатию пунктов меню.

---
+++ app/src/main/java/com/example/kruzyabra/myapplication/fragments/SendFragment.java
@@ -0,0 +1,19 @@
+package com.example.kruzyabra.myapplication.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kruzyabra.myapplication.R;
+
+public class SendFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_send, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/kruzyabra/myapplication/fragments/foo.java
@@ -0,0 +1,7 @@
+package com.example.kruzyabra.myapplication.fragments;
+
+/**
+ * Created by Kruzyabra on 11.08.2016.
+ */
+public class foo {
+}
