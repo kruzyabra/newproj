04cbc9cbc059ef1d448a137f0b5522461a546984
Kruzyabra <kruzyabra@yandex.ru>
2016-08-11 00:59:53 +0300

    ButterKnife в ImportFragment.java

--- app/src/main/java/com/example/kruzyabra/myapplication/fragments/ImportFragment.java
+++ app/src/main/java/com/example/kruzyabra/myapplication/fragments/ImportFragment.java
@@ -5,14 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.kruzyabra.myapplication.R;
 
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ImportFragment extends Fragment {
+
+    @BindView(R.id.textView) TextView mTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_import, container, false);
+        View source = inflater.inflate(R.layout.fragment_import, container, false);
+        ButterKnife.bind(this, source);
+        Random random = new Random(System.currentTimeMillis());
+        mTextView.setText(String.valueOf(random.nextInt(100)));
+        return source;
     }
 }
--- app/src/main/java/com/example/kruzyabra/myapplication/fragments/foo.java
+++
@@ -1,7 +0,0 @@
-package com.example.kruzyabra.myapplication.fragments;
-
-/**
- * Created by Kruzyabra on 11.08.2016.
- */
-public class foo {
-}
